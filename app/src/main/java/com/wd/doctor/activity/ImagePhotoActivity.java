e091393b81d5cbdd39273842ff00e87a888cd93c
1748624086 <1748624086@qq.com>
2019-12-19 21:48:26 +0800

    图片上传

--- app/src/main/java/com/wd/doctor/activity/ImagePhotoActivity.java
+++ app/src/main/java/com/wd/doctor/activity/ImagePhotoActivity.java
@@ -1,36 +1,50 @@
 package com.wd.doctor.activity;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bw.movie.ToastUtils;
 import com.bw.movie.base.BaseActivity;
 import com.bw.movie.base.BasePresenter;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.wd.doctor.HuanImageActivity;
 import com.wd.doctor.R;
+import com.wd.doctor.view.ImageUtil;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 
 public class ImagePhotoActivity extends BaseActivity {
 
-    @BindView(R.id.sim_pz)
-    SimpleDraweeView simPz;
+    @BindView(R.id.sim_pzimag)
+    ImageView simPz;
     @BindView(R.id.xxz)
     TextView xxz;
     @BindView(R.id.buton_wcsz)
     Button butonWcsz;
     private Dialog dialog;
     private View mInflate;
+    private MultipartBody.Part picture;
 
     @Override
     protected BasePresenter providePresenter() {
@@ -65,6 +79,7 @@ public class ImagePhotoActivity extends BaseActivity {
         Button mPic = mInflate.findViewById(R.id.set_but_pic);
         //取消
         Button mDiesmiss = mInflate.findViewById(R.id.set_but_dismiss);
+        Button xc = mInflate.findViewById(R.id.but_xc);
         /*点击*/
         //调起相机
         mPic.setOnClickListener(new View.OnClickListener() {
@@ -89,6 +104,22 @@ public class ImagePhotoActivity extends BaseActivity {
                 dialog.dismiss();
             }
         });
+
+        //相册
+        xc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK, null);
+                intent.setDataAndType(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        "image*/*");
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+                dialog.dismiss();
+            }
+
+        });
         //将布局设置给Dialog
         dialog.setContentView(mInflate);
         //获取当前Activity所在的窗体
@@ -106,53 +137,31 @@ public class ImagePhotoActivity extends BaseActivity {
 
 
 
-    /*public void show(View view) {
-        mDialog = new Dialog(this, R.style.DialogTheme);
-        //填充对话框的布局
-        mInflate = LayoutInflater.from(this).inflate(R.layout.set_ceshi, null);
-        //初始化控件
-        //拍照
-        mPic = mInflate.findViewById(R.id.set_but_pic);
-        //取消
-        mDiesmiss = mInflate.findViewById(R.id.set_but_dismiss);
-        //相册
-        mAlbum = mInflate.findViewById(R.id.set_but_album);
-        *//*点击*//*
-        //调起相机
-        mPic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //判断是不是选中图片了
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    //用一个工具类获取图片的绝对路径,我会粘到下方
+                    String path = ImageUtil.getPath(ImagePhotoActivity.this, uri);
+                    Glide.with(this).load(path)
+                            .into(simPz);
+                    if (path != null) {
+                        //转换为file类型
+                        File file = new File(path);
+                        //进行类型转换,因为在RetrofitService定义的是@Part MultipartBody.Part,所以要转成这样的格式
+                        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+                        picture = MultipartBody.Part.createFormData("image", file.getName(), requestBody);
+                    }
+                }
+            } else {
+                Toast.makeText(this, "取消相册", Toast.LENGTH_SHORT).show();
             }
-        });
-        //相册
-        mAlbum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                IntentUtils.getInstence().intentStart(SetImageActivity.this, ImagePhotoActivity.class);
-                finish();
-            }
-        });
-        //取消
-        mDiesmiss.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mDialog.dismiss();
-            }
-        });
-        //将布局设置给Dialog
-        mDialog.setContentView(mInflate);
-        //获取当前Activity所在的窗体
-        Window dialogWindow = mDialog.getWindow();
-        //设置Dialog从窗体底部弹出
-        dialogWindow.setGravity(Gravity.BOTTOM);
-        //获得窗体的属性
-        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
-        lp.y = 20;//设置Dialog距离底部的距离
-        //将属性设置给窗体
-        dialogWindow.setAttributes(lp);
-        mDialog.show();//显示对话框
-    }*/
+        }
+    }
 
 
     @Override
